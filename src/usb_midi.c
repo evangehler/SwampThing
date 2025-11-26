e65201a6d077fbff8949144d8a3c2b94b111d005
Evan Gehler <evangehler@gmail.com>
2025-11-25 22:23:03 -0500

    LED MIDI Flash

--- src/usb_midi.c
+++ src/usb_midi.c
@@ -3,6 +3,9 @@
 #include "synth.h"
 #include <math.h>
 
+#include "pico/stdlib.h"
+#include "bsp/board.h"
+
 #define A4_FREQ 440.0f
 #define A4_MIDI 69
 
@@ -20,15 +23,20 @@ void tud_midi_rx_cb(uint8_t itf) {
         uint8_t note   = packet[2];
         uint8_t vel    = packet[3];
 
+        // Handle Note On/Off
         if (status == 0x90 && vel > 0) {
+            // Note On: set frequency and turn LED on
             float freq = midi_to_freq(note);
             synth_set_frequency(freq);
             active_note = note;
+            board_led_write(true);
         }
         else if (status == 0x80 || (status == 0x90 && vel == 0)) {
+            // Note Off
             if (note == active_note) {
-                synth_gate_off();
+                synth_gate_off();               // Later
                 active_note = -1;
+                board_led_write(false);
             }
         }
     }
